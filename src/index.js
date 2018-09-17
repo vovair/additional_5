8250326cdc5c64bf63e5d37090561ad2fd33f3d3
vovair <vovair@gmail.com>
2018-09-18 01:53:55 +0300

    Added check function.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const openBrackets = bracketsConfig.reduce((ret, item) => {
+    ret[item[0]] = item[1];
+    return ret;
+  }, {});
+  const closedBrackets = bracketsConfig.reduce((ret, item) => {
+    ret[item[1]] = item[0];
+    return ret;
+  }, {});
+  for (letter of str) {
+    if (stack.length == 0) {
+      if (!openBrackets.hasOwnProperty(letter)) return false;
+      stack.push(letter);
+      continue;
+    }
+    if (closedBrackets.hasOwnProperty(letter)) {
+      if (closedBrackets[letter] == stack[stack.length - 1]) {
+        stack.pop();
+        continue;
+      }
+      if (!openBrackets.hasOwnProperty(letter)) return false;
+      stack.push(letter);
+      continue;
+    }
+    if (!openBrackets.hasOwnProperty(letter)) return false;
+    stack.push(letter);
+  }
+  return stack.length == 0;
+};
